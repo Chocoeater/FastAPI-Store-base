18cc3e1c97dfc5618891efae4cbdd542d325faaf
Chocoeater <lonelypain98@gmail.com>
2026-07-31 15:41:16 +0300

    add rating in product

--- app/models/products.py
+++ app/models/products.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Numeric, Boolean, ForeignKey
+from sqlalchemy import Integer, String, Numeric, Boolean, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from decimal import Decimal
 
@@ -17,6 +17,8 @@ class Product(Base):
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
     seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    rating: Mapped[float] = mapped_column(default=0.0, server_default=text('0'))
 
     category: Mapped["Category"] = relationship("Category", back_populates="products")
     seller: Mapped["User"] = relationship("User", back_populates="products")
+
